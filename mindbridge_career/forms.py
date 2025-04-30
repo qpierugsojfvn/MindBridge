41174764dfd3a0f709c680351df0d7540d2f856c
qpierugsojfvn <210103259@stu.sdu.edu.kz>
2025-04-30 12:03:09 +0500

    Altered MindBridge career app

--- mindbridge_career/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Application
-
-class ApplicationForm(forms.ModelForm):
-    class Meta:
-        model = Application
-        fields = ['cover_letter', 'resume']
