e585ab20a77acd9b15ba6d8d3e5908a71ca6707f
qpierugsojfvn <210103259@stu.sdu.edu.kz>
2025-05-08 07:45:17 +0500

    Added templates to discussion form, home, career home

--- mindbridge_library/urls.py
+++ mindbridge_library/urls.py
@@ -11,18 +11,3 @@ urlpatterns = [
     path('<slug:slug>/add-attachment/', views.add_attachment, name='add_attachment'),
     path('<slug:slug>/update-progress/', views.update_progress, name='update_progress'),
 ]
-
-
-# from django.urls import path
-# from . import views
-#
-# urlpatterns = [
-#     path('', views.index, name='library'),
-#     path('articles/', views.index, name='filter_articles'),
-#     path('videos/', views.index, name='filter_videos'),
-#     path('create/', views.create_lesson, name='create_lesson'),
-#     path('<slug:slug>/edit/', views.update_lesson, name='update_lesson'),
-#     path('<slug:slug>/', views.lesson_detail, name='lesson_detail'),
-#     path('<slug:slug>/add-attachment/', views.add_attachment, name='add_attachment'),
-#     path('<slug:slug>/update-progress/', views.update_progress, name='update_progress'),
-# ]
