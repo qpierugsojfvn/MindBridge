08da00f1c96046bb39c991dc79c7b80d3ffc7962
qpierugsojfvn <oarmanuly@jmart.kz>
2025-05-13 21:20:41 +0500

    Remade discussions

--- base/admin.py
+++ base/admin.py
@@ -3,3 +3,4 @@ from .models import *
 
 admin.site.register(Discussion)
 admin.site.register(Answer)
+admin.site.register(CustomTag)
--- base/models.py
+++ base/models.py
@@ -1,39 +1,21 @@
-from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from taggit.managers import TaggableManager
-from taggit.models import TagBase, GenericTaggedItemBase, TaggedItem
 
-from .utils import *
+class CustomTag(models.Model):
+    name = models.CharField(unique=True, max_length=100)
 
-
-class CustomTag(TagBase):
-    slug = models.SlugField(unique=True, max_length=100, allow_unicode=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = custom_slugify_(self.name)
-        super().save(*args, **kwargs)
-
-
-class TaggedItem(GenericTaggedItemBase):
-    tag = models.ForeignKey(
-        CustomTag,
-        related_name="%(app_label)s_%(class)s_items",
-        on_delete=models.CASCADE,
-    )
-
+    def __str__(self):
+        return self.name
 
 class Discussion(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = TaggableManager(through=TaggedItem, blank=True)
-    tagged_items = GenericRelation(TaggedItem, related_query_name='discussion')
+    tags = models.ManyToManyField(CustomTag)
 
     class Meta:
         ordering = ['-updated_at', '-created_at']
